669575688f0733d9074f2abc678539e9086400af
gitit4321 <gitit4321@gmail.com>
2021-02-14 17:36:53 -0500

    clean up

--- blazers_scraper.py
+++ blazers_scraper.py
@@ -10,29 +10,23 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 # locate table containing blazers stats
 blazers_table = soup.find(id='box-POR-game-basic')
-table_rows = blazers_table.tbody.find_all('tr')
 reserves_body = blazers_table.tbody.find('tr', class_='thead')
 rows = blazers_table.findAll('tr')[2:]
 
 # starter data extraction
 starter_headers = [th.getText() for th in blazers_table.find_all('th', class_='poptip')]
-
 starter_stats = [[td.getText() for td in rows[i].findAll('td')] for i in range(5)]
-
 starter_names = [[td.getText() for td in rows[i].findAll('th')] for i in range(5)]
-
-for i in range(len(starter_names)):
-    starter_stats[i].insert(0, starter_names[i][0])
+for i in range(len(starter_names)):                 
+    starter_stats[i].insert(0, starter_names[i][0])         # insert player names in 'starter_stats' list
 
 # reserve data extraction
 reserve_headers = [th.getText() for th in reserves_body.find_all('th')]
-
 reserve_stats = [[td.getText() for td in rows[i].findAll('td')] for i in range(6, len(rows) - 1)]
-
 reserve_names = [[td.getText() for td in rows[i].findAll('th')] for i in range(6, len(rows) - 1)]
 
 for i in range(len(reserve_names)):
-    reserve_stats[i].insert(0, reserve_names[i][0])
+    reserve_stats[i].insert(0, reserve_names[i][0])         # insert player names in 'reserve_stats' list
     
 # overall team stats
 team_stats = [[td.getText() for td in rows[-1].findAll('td')]]
@@ -40,7 +34,6 @@ team_stats[0].insert(0, "Team Totals")
 
 # pandas dataframe
 starter_dataFrame = pd.DataFrame(starter_stats, columns = starter_headers)
-
 reserve_dataFrame = pd.DataFrame(reserve_stats, columns = starter_headers)
 
 print(starter_dataFrame.head(5))
